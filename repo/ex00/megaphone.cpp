719a15966338fe20300a4a678ab07a9c027ebaad
Quentin Hauuy <quentinhauuy@hotmail.com>
2025-01-17 10:56:49 +0100

    blabla

---
+++ repo/ex00/megaphone.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: qhauuy <qhauuy@student.42mulhouse.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/14 14:12:05 by aperez-b          #+#    #+#             */
+/*   Updated: 2025/01/15 16:06:20 by qhauuy           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(int argc, char **argv)
+{
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			for (int j = 0; argv[i][j]; j++)
+				argv[i][j] = std::toupper(argv[i][j]);
+			std::cout << argv[i];
+		}
+		std::cout << std::endl;
+	}
+	return (0);
+}
